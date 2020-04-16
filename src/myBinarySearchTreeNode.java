1f5cc1ebe4d7a2d21e19a461af7e3fa9b4b34e00
Nadia <atiyeh.nadiaa@gmail.com>
2020-04-15 22:18:53 -0600

    Finished methods, and added Tester file

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -2,16 +2,22 @@ class myBinarySearchTreeNode{
   int myValue;
   myBinarySearchTreeNode left;
   myBinarySearchTreeNode right;
-    
+
+
   myBinarySearchTreeNode(int inValue){
     // created a new node with empty child pointers
-
+    this.myValue = inValue;
+    this.left = null;
+    this.right = null;
   }
   
   myBinarySearchTreeNode(int[] A){
     // creates a new Binary Search Tree rooted at the first value in the array
     /// by inserting elements into the tree in the order they are given in A.
-
+    myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A[0]);
+    for(int i = 1; i < A.length; i++) {
+      tree.insert(A[i]);
+    }
   }
   
   public void insert(int inValue){
@@ -21,14 +27,53 @@ class myBinarySearchTreeNode{
     //    * as the right child, 
     //    * in the left subtree,
     //    * or in the right subtree.
-    // If the value already exists in the tree, no action is taken. 
-    
+    // If the value already exists in the tree, no action is taken.
+    if(inValue > this.myValue){
+      if(this.right == null){
+        this.right = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        this.right.insert(inValue);
+      }
+    }
+    else if(inValue < myValue){
+      if(this.left == null){
+        this.left = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        this.left.insert(inValue);
+      }
+    }
   }
-  
+
   public int height(){
      // This method recursively calculates the height of the entire (sub)tree.
      // This method will take O(n) time
-     return -1;
+    int leftH = 0;
+    int rightH = 0;
+    if(this.left == null && this.right == null){
+      return 0;
+    }
+    else{
+      if(this.left != null){
+        leftH = this.left.height();
+      }
+      if(this.right != null){
+        rightH = this.right.height();
+      }
+      int height = 1 + findMax(leftH, rightH);
+      System.out.println(height);
+      return height;
+    }
+  }
+
+  private int findMax(int left, int right){
+    if(left > right){
+      return left;
+    }
+    else{
+      return right;
+    }
   }
   
   public int depth(int search){
@@ -37,11 +82,36 @@ class myBinarySearchTreeNode{
      // Note that if the tree is a proper BST, this method should complete in O(log n) time.
      // Additionally, remember that the depth is the number of nodes on the path from a node to the root 
      // (i.e. the number of the recursie calls).
+    if(search < this.myValue){
+      return this.left.depth(search) + 1;
+    }
+    if(search > this.myValue){
+      return this.right.depth(search) + 1;
+    }
+    if(search == myValue){
+      return 1;
+    }
     return -1;
+  }
+
+  public int size(){
+    // recursively calulates the amount of nodes in the tree
+    int size = 1;
+    if(left != null){
+      size += left.size();
+    }
+    if(right != null){
+      size += right.size();
+    }
+    System.out.println(size);
+    return size;
   }
   
   // Utility function included so you can debug your solution. 
-  public void print() { print(""); }
+  public void print() {
+    print("");
+  }
+
   private void print(String prefix) {
     System.out.println(prefix + myValue);
     prefix = prefix.replace('\u251C', '\u2502');
@@ -49,5 +119,4 @@ class myBinarySearchTreeNode{
     if(left != null) left.print(prefix + "\u251C ");
     if(right != null) right.print(prefix + "\u2514 ");
   }
-  
 } 
---
+++ src/myBinarySearchTreeNodeTester.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class myBinarySearchTreeNodeTester {
+
+    @Test
+    public void insertTester1(){
+
+    }
+
+    @Test
+    public void insertTester2(){
+
+    }
+
+    @Test
+    public void heightTester(){
+
+    }
+
+    @Test
+    public void depthTester(){
+
+    }
+
+    @Test
+    public void sizeTester({
+
+    }
+
+}

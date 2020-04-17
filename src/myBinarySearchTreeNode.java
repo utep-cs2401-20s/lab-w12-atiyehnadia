bc69e9b23c79247b25ad5d4c47601ab791b91685
Nadia <atiyeh.nadiaa@gmail.com>
2020-04-16 18:32:36 -0600

    Finshed methods and tester class

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -1,26 +1,27 @@
-class myBinarySearchTreeNode{
+class myBinarySearchTreeNode {
   int myValue;
   myBinarySearchTreeNode left;
   myBinarySearchTreeNode right;
 
 
-  myBinarySearchTreeNode(int inValue){
+  myBinarySearchTreeNode(int inValue) {
     // created a new node with empty child pointers
-    this.myValue = inValue;
-    this.left = null;
-    this.right = null;
+    myValue = inValue;
+    left = null;
+    right = null;
   }
-  
-  myBinarySearchTreeNode(int[] A){
+
+
+  myBinarySearchTreeNode(int[] A) {
     // creates a new Binary Search Tree rooted at the first value in the array
     /// by inserting elements into the tree in the order they are given in A.
-    myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A[0]);
-    for(int i = 1; i < A.length; i++) {
-      tree.insert(A[i]);
+    myValue = A[0];
+    for (int i = 1; i < A.length; i++) {
+      insert(A[i]);
     }
   }
-  
-  public void insert(int inValue){
+
+  public void insert(int inValue) {
     // This method takes a new integer as input and decides 
     // if it should be place: 
     //    * as the left child,
@@ -28,83 +29,87 @@ class myBinarySearchTreeNode{
     //    * in the left subtree,
     //    * or in the right subtree.
     // If the value already exists in the tree, no action is taken.
-    if(inValue > this.myValue){
-      if(this.right == null){
-        this.right = new myBinarySearchTreeNode(inValue);
+    if (inValue > myValue) {
+      if (right == null) {
+        right = new myBinarySearchTreeNode(inValue);
+      } else {
+        right.insert(inValue);
       }
-      else{
-        this.right.insert(inValue);
-      }
-    }
-    else if(inValue < myValue){
-      if(this.left == null){
-        this.left = new myBinarySearchTreeNode(inValue);
-      }
-      else{
-        this.left.insert(inValue);
+    } else if (inValue < myValue) {
+      if (left == null) {
+        left = new myBinarySearchTreeNode(inValue);
+      } else {
+        left.insert(inValue);
       }
     }
   }
 
-  public int height(){
-     // This method recursively calculates the height of the entire (sub)tree.
-     // This method will take O(n) time
+  public int height() {
+    // This method recursively calculates the height of the entire (sub)tree.
+    // This method will take O(n) time
     int leftH = 0;
     int rightH = 0;
-    if(this.left == null && this.right == null){
+    if (left == null && right == null) {
       return 0;
-    }
-    else{
-      if(this.left != null){
-        leftH = this.left.height();
+    } else {
+      if (left != null) {
+        leftH = left.height();
       }
-      if(this.right != null){
-        rightH = this.right.height();
+      if (right != null) {
+        rightH = right.height();
       }
       int height = 1 + findMax(leftH, rightH);
       return height;
     }
   }
 
-  private int findMax(int left, int right){
-    if(left > right){
+  private int findMax(int left, int right) {
+    if (left > right) {
       return left;
-    }
-    else{
+    } else {
       return right;
     }
   }
-  
-  public int depth(int search){
-     // This method recursively calculates the depth of a given search value. 
-     // If the given value is not in the tree, this method returns -1. 
-     // Note that if the tree is a proper BST, this method should complete in O(log n) time.
-     // Additionally, remember that the depth is the number of nodes on the path from a node to the root 
-     // (i.e. the number of the recursie calls).
-    if(search < this.myValue){
-      return this.left.depth(search) + 1;
-    }
-    if(search > this.myValue){
-      return this.right.depth(search) + 1;
-    }
-    if(search == this.myValue){
-      return 1;
+
+  public int depth(int search) {
+    // This method recursively calculates the depth of a given search value.
+    // If the given value is not in the tree, this method returns -1.
+    // Note that if the tree is a proper BST, this method should complete in O(log n) time.
+    // Additionally, remember that the depth is the number of nodes on the path from a node to the root
+    // (i.e. the number of the recursie calls).
+    int depth = 0;
+    if (search < myValue) {
+      if (left != null) {
+        if (left.depth(search) == -1) {
+          return -1;
+        }
+        return 1 + left.depth(search);
+      }
+    } else if (search > myValue) {
+      if (right != null) {
+        if (right.depth(search) == -1) {
+          return -1;
+        }
+        return 1 + right.depth(search);
+      }
+    } else if (search == myValue) {
+      return 0;
     }
     return -1;
   }
 
-  public int size(){
+  public int size() {
     // recursively calulates the amount of nodes in the tree
     int size = 1;
-    if(this.left != null){
+    if (left != null) {
       size += left.size();
     }
-    if(this.right != null){
-      size += this.right.size();
+    if (right != null) {
+      size += right.size();
     }
     return size;
   }
-  
+
   // Utility function included so you can debug your solution. 
   public void print() {
     print("");
@@ -114,7 +119,7 @@ class myBinarySearchTreeNode{
     System.out.println(prefix + myValue);
     prefix = prefix.replace('\u251C', '\u2502');
     prefix = prefix.replace('\u2514', ' ');
-    if(left != null) left.print(prefix + "\u251C ");
-    if(right != null) right.print(prefix + "\u2514 ");
+    if (left != null) left.print(prefix + "\u251C ");
+    if (right != null) right.print(prefix + "\u2514 ");
   }
-} 
+}
--- src/myBinarySearchTreeNodeTester.java
+++ src/myBinarySearchTreeNodeTester.java
@@ -5,29 +5,174 @@ import org.junit.jupiter.api.Test;
 
 public class myBinarySearchTreeNodeTester {
 
+
+    /*
+    TEST #1: INSERT
+    Tests the basic method functionality of the insert method, and takes the size of tree after the element is
+    inserted to see if was added correctly
+    INPUT: 1                 OUTPUT:  1
+             2                           2
+               3                            3
+                 4                             4
+                                                  5
+     INSERT: 5
+     EXPECTED SIZE: 5
+     Tests the insert when the value is greater than the root and will be inserted to the right
+     PASS/FAIL: PASS
+     */
+
     @Test
-    public void insertTester1(){
+    public void mBSTNTest1(){
+        int [] A = {1,2,3,4};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        tree.insert(5);
+        assertEquals(5, tree.size());
+    }
+
+    /*
+    TEST #2: INSERT VARIOUS
+    Tests the method functionality of the insert method, and takes the size of tree after the element is
+    inserted to see if was added correctly. This test will input various elements, larger numbers, and repeating #'s
+    INPUT:     1                  OUTPUT          1
+                  2                            0     2
+                     3                      -1         3
+                       4                                 4
+                                                           20
+     INSERT: 0, -1, 1, 20
+     EXPECTED SIZE: 7 (Excludes the repeated number)
+     Tests the insert when the value is less than the root and will be inserted to the left,
+     and greater than the root will be inserted to the right.
+     PASS/FAIL: PASS
+     */
+
+    @Test
+    public void mBSTNTest2(){
+        int [] A = {1,2,3,4};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        tree.insert(0);
+        tree.insert(-1);
+        tree.insert(1);
+        tree.insert(20);
+        assertEquals(7, tree.size());
 
     }
 
+     /*
+    TEST #3: HEIGHT
+    Tests the basic method functionality of the height method, and finds the height of the tree
+    INPUT:        2
+                    3
+                      4
+                        5
+                          8
+                            9
+                              14
+     EXPECTED HEIGHT: 6
+     Tests the height when the binary search tree has elements on either side of the tree, and looks for the
+     lowest leaf and counts the number of edges it takes to get to the root
+     PASS/FAIL: PASS
+     */
+
     @Test
-    public void insertTester2(){
+    public void mBSTNTest3(){
+        int [] A = {2,3,4,5,8,9,14};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        assertEquals(6, tree.height());
 
     }
 
+    /*
+    TEST #4: HEIGHT (COMPLEX TREE)
+    Tests the basic method functionality of the height method, and finds the height of the tree
+    when it has numbers branching both right and left
+    INPUT:                 20
+                     4           21
+                       6             23
+                 5       8        22
+                            9
+                              10
+
+     EXPECTED HEIGHT: 5
+     Tests the height when the binary search tree has elements on either side of the tree, and looks for the
+     lowest leaf and counts the number of edges it takes to get to the root
+     PASS/FAIL: PASS
+     */
+
     @Test
-    public void heightTester(){
+    public void mBSTNTest4(){
+        int [] A = {20,4,6,5,8,9,10,21,23,22};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        assertEquals(5, tree.height());
+    }
+
+    /*
+    TEST #5: DEPTH
+    Tests the basic method functionality of the depth method, and finds the location of an element
+    when the tree has numbers branching both right and left
+    INPUT:                9
+                        1   10
+                      2        20
+                    5             30
+                 6     3
+               8
+
+     Finds the element within the tree and then counts the number of edges between the root and the element.
+     ELEMENT: 3
+     EXPECTED DEPTH: 4
+     PASS/FAIL: PASS
+     */
+
+    @Test
+    public void mBSTNTest5(){
+        int [] A = {9,10,20,30,1,2,5,3,6,8};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        assertEquals(4, tree.depth(3));
 
     }
 
+     /*
+    TEST #6: DEPTH (NON-EXISTENT)
+    Tests the basic method functionality of the depth method, and finds the location of an element
+    when the tree has numbers branching both right and left
+    INPUT:                       9
+                        4               14
+                            6      11        20
+                        3                19
+
+     Finds the element within the tree and then counts the number of edges between the root and the element.
+     If element is non - existent it will out put -1
+     ELEMENT: 5
+     EXPECTED DEPTH: -1
+     PASS/FAIL: PASS
+     */
+
     @Test
-    public void depthTester(){
+    public void mBSTNTest6(){
+        int [] A = {9,14,11,20,4,6,19,3};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        assertEquals(-1, tree.depth(5));
 
     }
 
+    /*
+    TEST #:7 SIZE
+    Tests the basic method functionality of the size method, and finds keeps a running count of the total number of
+    nodes regardless of if the tree has a right or left subtree
+    INPUT:                 10
+                       9       11
+                    8               14
+                 3               13      20
+                   4                  18
+                 2
+               1
+     EXPECTED SIZE: 13
+     PASS/FAIL: PASS
+     */
+
     @Test
-    public void sizeTester({
-
+    public void mBSTNTest7(){
+        int [] A = {10,9,11,14,13,8,3,4,2,1,20,18};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+        assertEquals(12, tree.size());
     }
-
 }
